9a4f28f83ae4b53ca8697012e4d306d5cc9ef56e
João Vitor Ferreira <j.vitor.mf@gmail.com>
2021-02-23 16:41:12 -0300

    chore: separate routes

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,14 @@
-import express from 'express';
+import 'reflect-metadata';
+import express, { json } from 'express';
+
+import './database';
+
+import { routes } from './routes';
 
 const app = express();
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello World' })
-})
+app.use(express.json());
 
-app.post('/', (request, response) => {
-  return response.json({ message: 'Post com sucesso' })
-})
+app.use(routes);
 
 app.listen(3333, () => console.log('🚀 Server is running on port 3333!'));
