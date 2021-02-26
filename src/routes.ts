e9e646208aca856a12eae5d1187182840dfea866
João Vitor Ferreira <j.vitor.mf@gmail.com>
2021-02-26 18:30:39 -0300

    feat: create user survey to send by mail

--- src/routes.ts
+++ src/routes.ts
@@ -2,15 +2,19 @@ import { Router } from 'express';
 
 import { UserController } from './controllers/UserController';
 import { SurveyController } from './controllers/SurveyController';
+import { SendMailController } from './controllers/SendMailController';
 
 const routes = Router();
 
 const userController = new UserController();
 const surveyController = new SurveyController();
+const sendMailController = new SendMailController();
 
 routes.post('/users', userController.create);
 
 routes.get('/surveys', surveyController.index);
 routes.post('/surveys', surveyController.create);
+
+routes.post('/sendMail', sendMailController.execute);
 
 export { routes };
