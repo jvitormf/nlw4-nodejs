a8e0da6258c7c510698b17d49e8b44883aeb6a41
João Vitor Ferreira <j.vitor.mf@gmail.com>
2021-02-28 11:52:22 -0300

    chore: create custom Error class

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,29 @@
 import 'reflect-metadata';
-import express, { json } from 'express';
+import express, { json, NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
 
 import createConnection from './database';
 
 import { routes } from './routes';
+import { AppError } from './errors/AppError';
 
 createConnection();
 
 const app = express();
 
 app.use(json());
-
 app.use(routes);
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({ message: err.message });
+    }
+
+    return response.status(500).json({
+      status: 'Error',
+      message: `Internal Server Error ${err.message}`,
+    });
+  },
+);
 
 export { app };
